6f23739d790ca9fecc3d3d9c40041ae3dd948fc4
KimberlyChristensen <kimberly.christensen@du.edu>
2021-04-03 01:09:24 -0600

    updated js

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,4 @@
-
-//1. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+//Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 // Assign the value of the dropdown menu option to a variable
 
 function init() {
@@ -7,7 +6,7 @@ function init() {
     d3.json('/data/samples.json').then(data => {
         var name = data.names;
         var dropdownMenu = d3.select("#selDataset");
-        var selectedName = dropdownMenu.node().value.toString() == "940";
+        // var selectedName = dropdownMenu.node().value.toString() == "940";
 
         // Creates the dropdown list to choose from
         name.forEach((id) => {
@@ -29,12 +28,10 @@ function showCharts(selectedName) {
     // Use D3 to select the dropdown menu
     d3.json('/data/samples.json').then(data => {
         //Fill in the metadata
-
         var name = data.names;
         var metadata = data.metadata;
         var filteredMeta = metadata.filter(info => info.id == selectedName)[0];
 
-        console.log(filteredMeta)
         var tableInfo = d3.select("tbody");
         tableInfo.html("");
 
@@ -45,10 +42,6 @@ function showCharts(selectedName) {
         });
 
         //Create bar chart
-
-        // Use sample_values as the values for the bar chart.
-        // Use otu_ids as the labels for the bar chart.
-        // Use otu_labels as the hovertext for the chart.
 
         var samples = data.samples;
         var filteredSamples = samples.filter(info => info.id == selectedName)[0];
@@ -61,7 +54,7 @@ function showCharts(selectedName) {
 
         var trace = {
             x: sample_values.slice(0, 10).reverse(),
-            y: otu_id_label.slice(0, 10),
+            y: otu_id_label.slice(0, 10).reverse(),
             text: otu_labels.slice(0, 10),
             orientation: 'h',
             type: 'bar'
@@ -70,17 +63,11 @@ function showCharts(selectedName) {
 
         var layout = {
             title: "Top 10 OTUs Found",
-
         };
 
         Plotly.newPlot("bar", data, layout);
 
         //Create bubble chart
-        // Use otu_ids for the x values.
-        // Use sample_values for the y values.
-        // Use sample_values for the marker size.
-        // Use otu_ids for the marker colors.
-        // Use otu_labels for the text values.
 
         var trace1 = [{
             x: otu_ids,
@@ -103,56 +90,52 @@ function showCharts(selectedName) {
                 }
             }
         }
-        Plotly.newPlot('bubble', trace1, layout_bubble)
+        Plotly.newPlot('bubble', trace1, layout_bubble);
+
+        //Create gauge
+        var washFrequency = filteredMeta.wfreq;
+
+        var gauge_data = [
+            {
+                type: "indicator",
+                mode: "gauge+number",
+                domain: { 'x': [0, 1], 'y': [0, 1] },
+                value: washFrequency,
+                text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9"],
+                textInfo: "text",
+                textposition: "inside",
+                hoverinfo: "text",
+                gauge: {
+                    axis: { range: [null, 9], tickwidth: 1, tickcolor: "darkblue" },
+                    bar: { color: "deeppink"},
+                    borderwidth: 1,
+                    bordercolor: "white",
+                    steps: [
+                        { range: [0, 1], color: "oldlace" },
+                        { range: [1, 2], color: "papayawhip" },
+                        { range: [2, 3], color: "moccasin" },
+                        { range: [3, 4], color: "peachpuff" },
+                        { range: [4, 5], color: "mistyrose" },
+                        { range: [5, 6], color: "pink" },
+                        { range: [6, 7], color: "lightpink" },
+                        { range: [7, 8], color: "palevioletred" },
+                        { range: [8, 9], color: "mediumvioletred" },
+                    ],
+
+                }
+            }
+        ];
+
+        var gauge_layout = {
+            title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per Week', font: { size: 12 },
+            width: 500,
+            height: 400,
+            margin: { t: 30, r: 0, l: 0, b: 0 },
+        };
+    
+    Plotly.newPlot('gauge', gauge_data, gauge_layout);
     });
 };
 
-
-//4. Display the sample metadata, i.e., an individual's demographic information.
-
-// function showMetaData() {
-//     var tableInfo = d3.select("tbody");
-//     tableInfo.html("");
-
-//     d3.json('/data/samples.json').then(data => {
-//         var rawData = data;
-//         var name = data.names;
-
-//         var metadata = data.metadata;
-//         var metaID = data.metadata.id;
-//         var ethnicity = data.metadata.ethnicity;
-//         var gender = data.metadata.gender;
-//         var age = data.metadata.age;
-//         var location = data.metadata.location;
-//         var bbtype = data.metadata.bbtype;
-//         var wfreq = data.metadata.wfreq;
-
-
-
-//         var dropdownMenu = d3.select("#selDataset");
-//         var dropdownMenuID = dropdownMenu.id;
-//         var selectedOption = dropdownMenu.value;
-//         console.log(selectedOption);
-
-//         var selectedName = dropdownMenu.property("value");
-//         var filteredData = metadata.filter(demographics => demographics.id == selectedName);
-
-
-//         filteredData.forEach((one_row) => {
-//             var newrow = tableInfo.append('tr');
-//             Object.entries(filteredData[0]).forEach(([k, v]) => {
-//                 newrow.append('td').text(k, v);
-//             });
-//         });
-
-//     });
-// };
-
-// // showMetaData();
-
-
-// // 5. Display each key-value pair from the metadata JSON object somewhere on the page.
-
-// // 6. Update all of the plots any time that a new sample is selected
 
 init();
